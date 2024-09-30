87ef309b23aa80b2111cb3170cc4cbc3ae7122a8
carol <carol@Ordi-Carole>
2024-09-30 19:03:10 +0200

    Third commit, petit commentaire de classe

--- CogDistrib2024/src/cogDistrib2024/Grille.java
+++ CogDistrib2024/src/cogDistrib2024/Grille.java
@@ -1,5 +1,11 @@
 package cogDistrib2024;
-
+	/*
+	 * Classe qui représente une grille, dans chaque grille il y a un certains nombre d'éléments avec la présence
+	 * ou non de la cible. Les cases de grille contiennent soit un élément soit la valeur null.
+	 */
 public class Grille {
-
+	private int taille; 
+	private Element[][] grille;
+	private int nbrElement; 
+	private boolean presence; 
 }
--- CogDistrib2024/src/cogDistrib2024/Regard.java
+++ CogDistrib2024/src/cogDistrib2024/Regard.java
@@ -1,5 +1,9 @@
 package cogDistrib2024;
-
+/*
+ * La classe regard représente le "regard" du modèle. C'est ici qu'est calculer le temps de détection en fonction
+ * de la recherche danss la grille. 
+ */
 public class Regard {
-
+	private Grille grille; 
+	
 }
