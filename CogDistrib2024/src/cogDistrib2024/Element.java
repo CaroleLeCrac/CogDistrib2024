3444c5d702dd58bdb030491b8f09ea7e44cc9db1
carol <carol@Ordi-Carole>
2024-09-30 13:29:13 +0200

    First Commit

--- CogDistrib2024/src/cogDistrib2024/Element.java
+++ CogDistrib2024/src/cogDistrib2024/Element.java
@@ -1,5 +1,24 @@
 package cogDistrib2024;
-
+/*
+ * La classe qui représente les éléments à voir, soit des distracteur soit la cible
+ * Attribut : int etat (1 ou 2) 1la cible 2les distracteur
+ * 			  int similarité : représente la similarité entre les distracteur et la cible. + elle est élevé plus ils sont similaire
+ * 				à voir si similarité c'est pas plutôt un attribut à mettre pour la grille. 
+ */
 public class Element {
-
+	private int etat;
+	private int similarite; 
+	
+	public Element(int etat, int similarite) {
+		this.etat=etat;
+		this.similarite=similarite;
+	}
+	
+	public int get.etat(){
+		return etat;
+	}
+	
+	public int getSimilarite() {
+		return similarite; 
+	}
 }
